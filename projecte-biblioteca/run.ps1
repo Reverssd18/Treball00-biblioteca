feb1c45b6c8f93717157c419e617cf698334ded3
Victor Prieto <victorpg182006@gmail.com>
2025-01-20 14:25:17 +0100

    complete - run.ps1

---
+++ projecte-biblioteca/run.ps1
@@ -0,0 +1,5 @@
+# Cambiar al directorio donde está el proyecto de Maven
+Set-Location -Path "C:\Users\Victo\Documents\GitHub\Treball00-biblioteca\projecte-biblioteca"
+
+# Ejecutar Maven para ejecutar la clase principal utilizando el plugin maven-exec-plugin
+mvn exec:java
